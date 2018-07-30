a7d8e01175baaba5315e65432110a268da288b87
Sathappan MUTHIAH <sathap1@discovery1.cm.cluster>
2018-07-30 15:54:31 -0400

    updated script

--- results/deepgo/run_deepGO.sh
+++ results/deepgo/run_deepGO.sh
@@ -3,18 +3,31 @@
 ##SBATCH -p normal_q
 #SBATCH -p gpu_q
 #SBATCH -n 1
-#SBATCH -A fungcat
+##SBATCH -A fungcat
 ##SBATCH --nodelist hu008
 #SBATCH -t 360:00
 #SBATCH --mem=30G
-#SBATCH --gres=gpu:1
+## uncomment for huckleberry
+#SBATCH --gres=gpu:pascal:1
+## comment if not huckleberry
+##SBATCH --gres=gpu:1
 
+
+### Modules for huckleberry, uncomment accordingly
 #module load anaconda2
 #module load cuda
+
+### sathappans anaconda virtual environment
 #source activate pytorch
 
-module load TensorFlow/1.6.0-foss-2018a-Python-3.6.4-CUDA-9.1.85
-source ~/.start_discovery.sh
+## Modules for discovery
+#module load TensorFlow/1.6.0-foss-2018a-Python-3.6.4-CUDA-9.1.85
+#source ~/.start_discovery.sh
+
+
+
+
+#### Code for running python
 RESULTDIR="/home/sathap1/workspace/bioFunctionPrediction/results/deepgo/"
 SCRIPT_ROOT="/home/sathap1/workspace/bioFunctionPrediction/src/"
 cd $SCRIPT_ROOT
